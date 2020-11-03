81cd4724fca04e88386fecf3b4ea10630a6680b6
m5151128 <owatata344e@gmail.com>
2020-11-03 21:54:45 +0900

    session管理

--- flaskr/manage.py
+++ flaskr/manage.py
@@ -197,17 +197,15 @@ def get_post(id, check_author=True):
 
     return post
 
-# @bp.before_app_request
-# @app.before_app_request
+
+@app.before_request
 def load_logged_in_user():
     user_id = session.get('user_id')
 
     if user_id is None:
         g.user = None
     else:
-        g.user = get_db().execute(
-            'SELECT * FROM user WHERE id = ?', (user_id,)
-        ).fetchone()
+        g.user = User.query.filter_by(id=user_id).first()
 
 
 # @bp.route('/logout')
