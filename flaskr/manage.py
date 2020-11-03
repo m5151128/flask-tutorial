9a1fb1a486f1a5ee2bb0444dfeddf99235360be5
m5151128 <owatata344e@gmail.com>
2020-11-03 23:22:14 +0900

    login処理をSQLAlchemyに

--- flaskr/manage.py
+++ flaskr/manage.py
@@ -148,26 +148,23 @@ def register():
     return render_template('auth/register.html')
 
 
-# @bp.route('/login', methods=('GET', 'POST'))
 @app.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        db = get_db()
         error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
+
+        user = User.query.filter_by(name=username).first()
 
         if user is None:
             error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
+        elif not check_password_hash(user.password, password):
             error = 'Incorrect password.'
 
         if error is None:
             session.clear()
-            session['user_id'] = user['id']
+            session['user_id'] = user.id
             return redirect(url_for('index'))
 
         flash(error)
