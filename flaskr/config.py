2bfadfcc5684771e0f36412d96aaf5ca851d884a
m5151128 <owatata344e@gmail.com>
2020-11-03 15:42:13 +0900

    flask-migrateでテーブルを用意できるように

---
+++ flaskr/config.py
@@ -0,0 +1,7 @@
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/{db_name}?charset=utf8'.format(**{
+      'user': "root",
+      'password': "root",
+      'host': "db",
+      'db_name': "flaskr"
+})
+SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ flaskr/manage.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+
+class User(db.Model):
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.Text, nullable=False)
+
+
+class Post(db.Model):
+
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    title = db.Column(db.Text, nullable=False)
+    body = db.Column(db.Text, nullable=False)
+
+
+if __name__ == '__main__':
+    manager.run()
