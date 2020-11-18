ccbe36c8f36a48c7d6cde4da0675c533093edd0c
m5151128 <owatata344e@gmail.com>
2020-11-18 13:49:28 +0900

    modify change author_id to user_id

--- flaskr/manage.py
+++ flaskr/manage.py
@@ -34,7 +34,7 @@ class Post(db.Model):
     __tablename__ = 'posts'
 
     id = db.Column(db.Integer, primary_key=True)
-    author_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     title = db.Column(db.Text, nullable=False)
     body = db.Column(db.Text, nullable=False)
