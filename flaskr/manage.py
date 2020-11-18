ee7b41b00a0cef1714bc42d7eeb232691f3ff1a0
m5151128 <owatata344e@gmail.com>
2020-11-18 14:54:00 +0900

    add updated_at column and create revision

--- flaskr/manage.py
+++ flaskr/manage.py
@@ -28,6 +28,7 @@ class User(db.Model):
     name = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 
 class Post(db.Model):
@@ -37,6 +38,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     title = db.Column(db.Text, nullable=False)
     body = db.Column(db.Text, nullable=False)
 
