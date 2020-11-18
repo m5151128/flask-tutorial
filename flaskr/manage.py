369eed509e86f42edbbf6eadeaa102687c1efcc2
m5151128 <owatata344e@gmail.com>
2020-11-18 14:34:05 +0900

    add created_at to users table and db migrate

--- flaskr/manage.py
+++ flaskr/manage.py
@@ -27,6 +27,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 
 class Post(db.Model):
