9a5d0172296a20c4c36a64275bdf8734a98faede
m5151128 <owatata344e@gmail.com>
2020-11-18 13:36:45 +0900

    create new revisionfile command: 'db revision'

---
+++ flaskr/migrations/versions/050dddb4596b_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 050dddb4596b
+Revises: 47224eeeced1
+Create Date: 2020-11-18 04:33:40.259900
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '050dddb4596b'
+down_revision = '47224eeeced1'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
