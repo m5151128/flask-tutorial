cf1297cd70464b9b80e60fa59a9a01b78b644147
m5151128 <owatata344e@gmail.com>
2020-11-20 11:20:57 +0900

    create new revision file

---
+++ flaskr/migrations/versions/6b15f8547990_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 6b15f8547990
+Revises: bc70d70f45d1
+Create Date: 2020-11-20 02:20:29.444922
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6b15f8547990'
+down_revision = 'bc70d70f45d1'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
